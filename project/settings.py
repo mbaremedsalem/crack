48abd2450359e6cdad76c20aaffcbd8665aed60e
mbaremedsalem <mbaremedsalemmbare@gmail.com>
2026-06-08 12:38:55 -0700

    add files

--- project/settings.py
+++ project/settings.py
@@ -117,9 +117,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
+
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+# Ajoutez ceci pour les fichiers statiques supplémentaires
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',  # Si vous avez un dossier static à la racine
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
